abcef1669e0925a0275ada7c299b49d55e82db96
sandyLohran <sandylohran@gmail.com>
2023-05-09 22:28:10 -0300

    Comecando um projeto react!

---
+++ organo/src/componentes/banner/banner.js
@@ -0,0 +1,9 @@
+import './banner.css'
+function Banner() {
+    return (
+        <header className='banner'>
+        <img  src="/imagens/banner.png" alt="O banner principal da página do Organo."></img>
+        </header>
+    )
+}
+export default Banner
